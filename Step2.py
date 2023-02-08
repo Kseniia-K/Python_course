6d000de62dae52ed47054f88f836f59aa4a31352
Kseniia <kvxoxlova2005@mail.ru>
2023-02-08 11:40:07 +0300

    Not finished

---
+++ Step2.py
@@ -0,0 +1,108 @@
+# answer = input('Какой язык программирования мы изучаем?')
+# if answer == 'Python':
+#     print('Верно! Мы ботаем Python =)')
+#     print('Python - отличный язык!')
+# else:
+#     print('Не совсем так!')
+
+# оператор присваивания =
+
+# num = 1992
+# s = 'I love Python'
+
+# Для проверки двух элементов на равенство используется оператор ==
+
+# if answer == 'Python'
+
+# основные операторы сравнения
+
+# if x > 7
+# if x < 7
+# if x >= 7
+# if x <= 7
+# if x == 7
+# if x != 7
+
+# num1 = int(input())
+# num2 = int(input())
+#
+# if num1 < num2:
+#     print(num1, 'меньше чем', num2)
+# if num1 > num2:
+#     print(num1, 'больше чем', num2)
+# if num1 == num2:
+#     print(num1, 'равно', num2)
+# if num1 != num2:
+#     print(num1, 'не равно', num2)
+
+# цепочки сравнений
+
+# age = int(input())
+# if 3 <= age <= 6:
+#     print('Вы ребёнок')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# if a == b == c:
+#     print('Числа равны')
+# else:
+#     print('Числа не равны')
+
+# транзитивность, если a == b и b == c, то a == c
+
+# word = input()
+# if word== 'Python':
+#     print('Да')
+# else:
+#     print('Нет')
+
+# num = int(input())
+# last_digit =  num % 10
+# first_digit = num // 10
+# if last_digit == first_digit:
+#     print('Да')
+# else:
+#     print('Нет')
+
+# num1, num2, num3 = int(input()), int(input()), int(input())
+# counter = 0
+# if num1 % 2 == 0:
+#     counter = counter + 1
+# if num2 % 2 == 0:
+#     counter = counter + 1
+# if num3 % 2 == 0:
+#     counter = counter + 1
+# print(counter)
+
+# str1 = input()
+# str2 = input()
+# if str1 == str2:
+#     print('Пароль принят')
+# else:
+#     print('Пароль не принят')
+
+# num = int(input())
+# if num % 2 == 0:
+#     print('Четное')
+# else:
+#     print('Нечетное')
+
+# num = int(input())
+# last_digit = num % 10
+# third_digit = (num % 100) // 10
+# second_digit = (num // 100) % 10
+# first_digit = num // 1000
+#
+# if first_digit + last_digit == second_digit - third_digit:
+#     print('ДА')
+# else:
+#     print('НЕТ')
+
+# age = int(input())
+# if age >= 18:
+#     print('Доступ разрешен')
+# else:
+#     print('Доступ запрещен')
+
